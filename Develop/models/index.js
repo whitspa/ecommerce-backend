6d260e3adad2435e3a0414b623bd9c1400d50869
whit <whitspa64@gmail.com>
2022-12-27 21:12:48 -0500

    continued to add keys to models.worked on relationships betwen models

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -5,12 +5,34 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-
+Product.belongsTo(Category, {
+  foreignKey: 'Category_id',
+  onDelete: 'CASCADE',
+});
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'Category_id',
+  onDelete: 'CASCADE',
+});
 
 // Products belongToMany Tags (through ProductTag)
-
+Product.belongsToMany(Tag, {
+  through: {
+    model: ProductTag,
+    unique: true //do I even need this line?
+  },
+  // Define an alias for when data is retrieved. Do I need this Line?
+  as: 'Product_Tag'
+});
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: {
+    model: ProductTag,
+    unique: true //do I even need this line?
+  },
+  // Define an alias for when data is retrieved. Do I need this Line?
+  as: 'Product_Tag'
+});
 
 module.exports = {
   Product,
