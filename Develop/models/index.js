cf0d7709fa432d063be9cb6b816c3d296548b261
whit <whitspa64@gmail.com>
2023-01-06 14:03:02 -0500

    corrected syntax,relationships,and ids

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -6,12 +6,12 @@ const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
 Product.belongsTo(Category, {
-  foreignKey: 'Category_id',
+  foreignKey: 'category_id',
   onDelete: 'CASCADE',
 });
 // Categories have many Products
 Category.hasMany(Product, {
-  foreignKey: 'Category_id',
+  foreignKey: 'category_id',
   onDelete: 'CASCADE',
 });
 
@@ -19,19 +19,17 @@ Category.hasMany(Product, {
 Product.belongsToMany(Tag, {
   through: {
     model: ProductTag,
-    unique: true //do I even need this line?
   },
+  foreignKey: 'product_id',
   // Define an alias for when data is retrieved. Do I need this Line?
-  as: 'Product_Tag'
 });
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product, {
   through: {
     model: ProductTag,
-    unique: true //do I even need this line?
   },
+  foreignKey: "tag_id"
   // Define an alias for when data is retrieved. Do I need this Line?
-  as: 'Product_Tag'
 });
 
 module.exports = {
